2ed00e0ad948b45a6e6a8ccf7c5a899f60121986
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2022-12-16 11:20:58 +0600

    Update

--- app.py
+++ app.py
@@ -4,35 +4,37 @@ from flask_cors import CORS
 import requests
 
 def img_to_text(base64:str):
-    attemp=0
-    while attemp<100:
-        resp = requests.post(
-            url="https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAUbA4T8UWO-pw750uQqz0X2deq9lHLuLk",
-            headers={
-                "x-android-package": "image.to.text.ocr",
-                "x-android-cert": "ad32d34755bb3b369a2ea8dfe9e0c385d73f80f0",
-                "Content-Type": "application/json",
-            },
-            data='{"requests": [{"features": [{"maxResults": 10,"type": "DOCUMENT_TEXT_DETECTION"}],"image": {"content": "' + base64 + '"}}]}',
-        )
-        try:
-            return resp.json()["responses"][0]["fullTextAnnotation"]["text"]
-        except:
-            if 'Resource' in resp.text:
-                attemp+=1
-                continue
-            elif 'Quota exceeded' in resp.text:
-                attemp+=1
-                continue
-            else:
-                raise Exception(f'Something went wrong! {resp.text}')
+	attemp=0
+	while attemp<100:
+		resp = requests.post(
+			url="https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAUbA4T8UWO-pw750uQqz0X2deq9lHLuLk",
+			headers={
+				"x-android-package": "image.to.text.ocr",
+				"x-android-cert": "ad32d34755bb3b369a2ea8dfe9e0c385d73f80f0",
+				"Content-Type": "application/json",
+			},
+			data='{"requests": [{"features": [{"maxResults": 10,"type": "DOCUMENT_TEXT_DETECTION"}],"image": {"content": "' + base64 + '"}}]}',
+		)
+		try:
+			return resp.json()["responses"][0]["fullTextAnnotation"]["text"]
+		except:
+			if 'Resource' in resp.text:
+				attemp+=1
+				continue
+			elif 'Quota exceeded' in resp.text:
+				attemp+=1
+				continue
+			else:
+				raise Exception(f'Something went wrong! {resp.text}')
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='thisismysecretkey'
 CORS(app)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
+	if request.method=='GET':
+		return 'Working'
 	base64=request.form.get('data')
 	try:
 		text=img_to_text(base64)
